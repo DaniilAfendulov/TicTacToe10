d6f156b21da867e8532f59949155ac431be0e3b6
daniilafendulov <daniilafendulov@bk.ru>
2023-01-20 05:44:13 +0300

    Add Initializtion and MakeMove tests

---
+++ TicTacToeTests/BoardTests.cs
@@ -0,0 +1,54 @@
+using TicTacToeLogic;
+using TicTacToeLogic.Interfaces;
+
+namespace TicTacToeTests
+{
+    public class BoardTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetBoard_InitializtionBoard_ReturnsArrayOfNull10x10()
+        {
+            var expected = new MoveEnum?[10, 10];
+            ITicTacToeBoard board = new TicTacBoard10();
+            var actual = board.GetBoard();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TryMakeMove_MoveX_ReturnsArrayWithX()
+        {
+            var expected = new MoveEnum?[10, 10];
+            expected[0, 0] = MoveEnum.X;
+
+            ITicTacToeBoard board = new TicTacBoard10();
+            board.TryMakeMove(0, 0, MoveEnum.X);
+            var actual = board.GetBoard();
+            CollectionAssert.AreEqual(expected, actual);
+
+            expected[5, 5] = MoveEnum.X;
+            board.TryMakeMove(5, 5, MoveEnum.X);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TryMakeMove_MoveO_ReturnsArrayWithO()
+        {
+            var expected = new MoveEnum?[10, 10];
+            expected[0, 0] = MoveEnum.O;
+
+            ITicTacToeBoard board = new TicTacBoard10();
+            board.TryMakeMove(0, 0, MoveEnum.O);
+            var actual = board.GetBoard();
+            CollectionAssert.AreEqual(expected, actual);
+
+            expected[5, 5] = MoveEnum.O;
+            board.TryMakeMove(5, 5, MoveEnum.O);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
