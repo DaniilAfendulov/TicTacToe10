13a5e661c4af776bd9bc3949d8eda3c5923b531f
daniilafendulov <daniilafendulov@bk.ru>
2023-01-20 13:14:22 +0300

    Add Game UI

--- TicTacToe10WinApp/Form1.Designer.cs
+++ TicTacToe10WinApp/Form1.Designer.cs
@@ -28,12 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 426);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
     }
 }
--- TicTacToe10WinApp/Form1.cs
+++ TicTacToe10WinApp/Form1.cs
@@ -1,10 +1,108 @@
+using TicTacToeLogic;
+using TicTacToeLogic.Interfaces;
+
 namespace TicTacToe10WinApp
 {
     public partial class Form1 : Form
     {
+        private ITicTacToeBoard _board;
         public Form1()
         {
             InitializeComponent();
+            CreateBoard();
         }
+
+        private void CreateBoard()
+        {
+            _board = new TicTacBoard10();
+            _board.OnGameEnd += OnGameEndHandler;
+
+            tableLayoutPanel1.ColumnCount = 10;
+            tableLayoutPanel1.RowCount = 10;
+            tableLayoutPanel1.Controls.Clear();
+            tableLayoutPanel1.RowStyles.Clear();
+            tableLayoutPanel1.ColumnStyles.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                tableLayoutPanel1.RowStyles.Add(new RowStyle() { SizeType = SizeType.Percent, Height = 10 });
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle() { SizeType = SizeType.Percent, Width = 10 });
+                for (int j = 0; j < 10; j++)
+                {
+                    var btn = CreateButton(i, j);
+                    tableLayoutPanel1.Controls.Add(btn);
+                }
+            }
+        }
+
+        private void OnGameEndHandler(object? sender, GameEndEventArgs e)
+        {
+            string text = "";
+            switch (e.FinalMove)
+            {
+                case GameResultEnum.WinX:
+                    text = "ÂÛÈÃÐÀË X";
+                    break;
+                case GameResultEnum.WinO:
+                    text = "ÂÛÈÃÐÀË O";
+                    break;
+                case GameResultEnum.Draw:
+                    text = "ÍÈ×Üß";
+                    break;
+            }
+            MessageBox.Show(text);
+
+            if (MessageBox.Show("Ïåðåçàïóñòèòü èãðó") == DialogResult.OK)
+            {
+                _board.OnGameEnd -= OnGameEndHandler;
+                CreateBoard();
+            }
+        }
+
+        private Button CreateButton(int x, int y)
+        {
+            var btn = new Button()
+            {
+                Anchor = AnchorStyles.Top,
+                Dock = DockStyle.Fill,
+                AutoSize = true,
+                TextAlign = ContentAlignment.MiddleCenter,
+            };
+            btn.Click += BtnClickHandler(x, y);
+            return btn;
+        }
+
+        private EventHandler BtnClickHandler(int x, int y)
+        {
+            return (sender, eventArgs) =>
+            {
+                if (_board.TryMakeMove(x, y))
+                {
+                    var move = _board.GetBoard()[x, y];
+                    if (sender is Button)
+                    {
+                        PaintButton((Button)sender, move);
+                    }
+                }
+            };
+        }
+
+        private void PaintButton(Button button, MoveEnum? move)
+        {
+            string text = "";
+            switch (move)
+            {
+                case MoveEnum.X:
+                    text = "X";
+                    break;
+                case MoveEnum.O:
+                    text = "O";
+                    break;
+            }
+            button.Text = text;
+            button.Enabled = false;
+        }
+
+        
+
     }
 }
